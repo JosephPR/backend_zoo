0c547f50fe60e1daf1b555284fa1931097fbcbe9
JosephPR <reilly4181@gmail.com>
2019-09-09 22:31:09 -0600

    animals table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Animal.destroy_all
+
+Animal.create(name: "Bear")
+Animal.create(name: "Tiger")
+Animal.create(name: "Elephant")
+Animal.create(name: "Peacock")
+Animal.create(name: "Monkey")
